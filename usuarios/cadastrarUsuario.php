76be027a9a674fba946579921ab90121271d22de
PedroFranzoi <pedro_franzoi@estudante.sesisenai.org.br>
2025-10-30 10:10:51 -0300

    add - arquivo de cadastro de usuario

---
+++ usuarios/cadastrarUsuario.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar usuarios</title>
+</head>
+<body>
+    <h1>Cadastrar Usuario</h1>
+
+    <form method="POST" action="">
+
+    <label for="nome">Nome:</label>
+    <input type="text" name="nome" required>
+    <br>
+    <label for="nome">Email:</label>
+    <input type="text" name="email" required>
+    <br>
+    <br>
+
+    <button type="submit">Criar</button>
+    <br>
+    <br>
+    <a href="../index.php">Voltar</a>
+
+    </form>
+
+
+
+</body>
+</html>
